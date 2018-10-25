1e8d835b8905e8554aeccf7540cd8b986c5f3f20
Arsile <arsile410@gmail.com>
2018-10-25 21:32:13 +0300

    productionDeployFix2
    added missing referenceTo MySql
    fixed webpack.config.js

--- EShop/angular/webpack.config.js
+++ EShop/angular/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     new webpack.DefinePlugin({
       // global app config object
       config: JSON.stringify({
-        apiUrl: 'http://localhost:4000'
+        apiUrl: 'http://149.154.64.172:4000'
       })
     })
   ],
